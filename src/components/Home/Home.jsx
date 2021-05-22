76d9eb50a5bd3d7233a08ae528110c690509d4bf
Artem Onufrei <2hilderoy2@gmail.com>
2021-05-22 23:17:06 +0300

    Added basic components for routes

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,8 +1,48 @@
-import React from 'react';
+import React from "react";
+
+import clsx from "clsx";
+import Typography from "@material-ui/core/Typography";
+import Divider from "@material-ui/core/Divider";
+import Container from "@material-ui/core/Container";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import { makeStyles } from "@material-ui/core/styles";
+import { useStyles } from "../mainStyles";
+import { listGenerator } from "../lists";
+import * as constants from "../constants";
+
+const useHomeStyles = makeStyles((theme) => ({
+    title: {
+        marginBottom: theme.spacing(4),
+    },
+    modelListTitle: {
+        padding: theme.spacing(3),
+    }
+}));
 
 export default function Home() {
+    const mainClasses = useStyles();
+    const classes = useHomeStyles();
+    const fixedHeightPaper = clsx(mainClasses.paper, mainClasses.fixedHeight);
+
     return (
-        <>
-        </>
-    )
-} 
+        <Container maxWidth="md" className={mainClasses.container}>
+            <Typography variant="h3" component="h2" className={classes.title}>
+                Hello
+            </Typography>
+            <Grid container spacing={3}>
+                <Grid item xs={8}></Grid>
+                <Grid item xs={4}>
+                    <Paper className={classes.paper}>
+                        <Typography variant='h6' className={classes.modelListTitle}>Choose your model</Typography>
+                        <Divider />
+                        {listGenerator(constants.models)}
+                    </Paper>
+                </Grid>
+            </Grid>
+        </Container>
+    );
+}
+
+//            <Typography variant="h3" component="h2" className={classes.title}>Hello</Typography>
+//            <Typography variant="h5" component="h3">This is building organization application. Developed by Artem Onufrei</Typography>
--- src/components/Models/Models.jsx
+++ src/components/Models/Models.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
 
+import Container from '@material-ui/core/Container';
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from '@material-ui/core/styles';
+import { useStyles } from "../mainStyles";
+
+const useModelsStyles = makeStyles((theme) => ({
+    title: {
+        marginBottom: theme.spacing(4),
+    }
+}));
+
 export default function Models() {
+    const mainClasses = useStyles();
+    const classes = useModelsStyles();
+
     return (
-        <>
-        </>
+        <Container maxWidth="md" className={mainClasses.container}>
+            <Typography variant="h4" component="h4" className={classes.title}>Choose Model</Typography>
+        </Container>
     )
 }
