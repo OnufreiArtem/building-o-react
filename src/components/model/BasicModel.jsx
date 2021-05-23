df4b5c2339f1fe1cba22afb99edeaab8322c9831
Artem Onufrei <2hilderoy2@gmail.com>
2021-05-23 09:52:50 +0300

    Added auto table from json

--- src/components/model/BasicModel.jsx
+++ src/components/model/BasicModel.jsx
@@ -1,7 +1,28 @@
-import React from 'react';
+import React from "react";
 
-export default function BasicModelTable(data) {
+import MaterialTable from "material-table";
+import Container from "@material-ui/core/Container"
+export default function BasicModelTable({data}) {
+    function capitalize(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+
+    const generateCols = (keys) => {
+        const result = keys.map((key) => ({ title: capitalize(key), field: key }));
+        return result.filter( r => r.title !== "CreatedAt" && r.title !== "ModifiedAt" )
+    }
+        
+
     return (
-        <> </>
+        <Container width="sm">
+            <MaterialTable
+                columns={generateCols(Object.keys(!data || !data[0] ? {} : data[0]))}
+                options={{
+                    selection: true,
+                }}
+                data={data}
+                title="Some Title"
+            />
+        </Container>
     );
 }
